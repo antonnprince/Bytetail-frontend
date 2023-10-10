e4f6d98010122953b137d617e0c5d750091ca8f1
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2023-10-11 00:56:54 +0530

    Add files via upload

---
+++ aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+  aws_project_region: 'Sydney',
+  aws_cognito_identity_pool_id: 'YOUR_IDENTITY_POOL_ID',
+  aws_cognito_region: 'YOUR_AWS_REGION',
+  aws_user_pools_id: 'YOUR_USER_POOL_ID',
+  aws_user_pools_web_client_id: 'YOUR_APP_CLIENT_ID',
+};
+
+export default awsmobile;
